eba7e2973b9c7a4824db7a981ff0307dde769733
Chernenko Kateryna <Lukum30@yandex.ru>
2018-08-13 18:03:53 +0300

    Add files via upload

---
+++ ToggleButtons/ToggleButtons/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToggleButtons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            /*if(TB1.IsChecked==true)
+            {
+
+            }
+            else
+            {
+
+            }*/
+        }
+
+        public void toOn(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Works");
+        }
+    }
+}
